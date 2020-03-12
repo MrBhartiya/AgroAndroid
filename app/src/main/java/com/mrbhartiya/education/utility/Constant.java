19eb06867e793a8183812151245c959052b1a1eb
Manoj <mrbhartiya.server@gmail.com>
2020-03-13 01:13:59 +0530

    ///UPDATE

--- app/src/main/java/com/mrbhartiya/education/utility/Constant.java
+++ app/src/main/java/com/mrbhartiya/education/utility/Constant.java
@@ -7,7 +7,8 @@ import java.io.File;
 public class Constant {
 
     //public static final String BASE_URL = "http://ec2-13-232-236-23.ap-south-1.compute.amazonaws.com/api/";
-    public static final String BASE_URL = "http://ec2-13-232-236-23.ap-south-1.compute.amazonaws.com/api/";
+  //  public static final String BASE_URL = "http://ec2-13-232-236-23.ap-south-1.compute.amazonaws.com/api/";
+    public static final String BASE_URL = "http://api.mrbhartiya.com/api/";
     public static final int MIN_BUFFER_DURATION = 10;
     public static final String STORAGE_LOCATION = Environment.getExternalStorageDirectory() + File.separator + ".mbr/";
     public static final String CLIENT_KEY = PreferenceHelper.getClientKey();
